628ad226d6bcae2c5f0300b5ab1c5558e6c9a181
Audrius Idzelis <67578125+aidzelis-verimatrix@users.noreply.github.com>
2023-08-04 13:22:11 +0300

    chore: updated env variables after documentation update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-python3 /aps-cli/aps.py -v -c "$APS_KEY" -s "$APS_SECRET" protect --file "$APP_FILE"
+python3 /aps-cli/aps.py -v -c "$API_KEY_ID" -s "$API_SECRET" protect --file "$APP_FILE"
 
 RESULT_FILE=protect_result.txt
 if [ -f "$RESULT_FILE" ]; then
