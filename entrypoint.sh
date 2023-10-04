45eeb14ed3d1a8c6e3b4ba980493734453b42ae9
Audrius Idzelis <67578125+aidzelis-verimatrix@users.noreply.github.com>
2023-10-04 10:41:05 +0300

    chore: updating to support setting subscription type

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -l
 
-python3 /aps-cli/aps.py -v -c "$API_KEY_ID" -s "$API_SECRET"  --api-gateway-url "$API_GATEWAY_URL" --access-token-url "$ACCESS_TOKEN_URL" protect --file "$APP_FILE"
+# retrieving account info for getting subscription_type if it's not provided
+if [[ -z ${SUBSCRIPTION_TYPE}]]; then
+  python3 /aps-cli/aps.py -v -c "$API_KEY_ID" -s "$API_SECRET"  --api-gateway-url "$API_GATEWAY_URL" --access-token-url "$ACCESS_TOKEN_URL" get-account-info > account.info
+  cat account.info
+  SUBSCRIPTION_TYPE = jq '.["customer"]["subscriptions"][0]["type"]'
+  echo "Subscription Type retrieved [$SUBSCRIPTION_TYPE]"
+fi
+
+python3 /aps-cli/aps.py -v -c "$API_KEY_ID" -s "$API_SECRET"  --api-gateway-url "$API_GATEWAY_URL" --access-token-url "$ACCESS_TOKEN_URL" protect --subscription-type "$SUBSCRIPTION_TYPE" --file "$APP_FILE"
 
 RESULT_FILE=protect_result.txt
 if [ -f "$RESULT_FILE" ]; then
