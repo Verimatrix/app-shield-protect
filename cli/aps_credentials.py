4c8dbb7175d7fd204edea478b8bcaaf714ecb4bd
Audrius Idzelis <67578125+aidzelis-verimatrix@users.noreply.github.com>
2023-10-17 11:46:42 +0300

    fix: updated to support platform API keys

--- cli/aps_credentials.py
+++ cli/aps_credentials.py
@@ -13,27 +13,37 @@ LOGGER = logging.getLogger(__name__)
 
 
 def authenticate_api_key(api_key_id, api_key, config, vmx_platform, **kwargs):
-    '''Authentication using an API Key. Uses an API Key ID and API Key.
-        Returns an token that can be used as a HTTP authorization header'''
+    '''Authentication using an API Key. sReturns an token that can be used as a HTTP authorization header'''
+
+    resp = {}
     if vmx_platform:
-        raise ApsException('Client ID and Secret authentication not supported for Verimatrix Platform')
+        LOGGER.debug('Authenticating with platform API key')
+        url = config['platform-access-token-url']
+        body = {}
+        body['userEmail'] = api_key_id
+        body['apiKey'] = api_key
 
-    msg = f'{api_key_id}:{api_key}'
-    auth = base64.b64encode(msg.encode('ascii')).decode('ascii')
+        response = ApsRequest.post(url, json={'body': body})
+        resp = response.json()
+    else:
+        LOGGER.debug('Authenticating with client credentials')
+        msg = f'{api_key_id}:{api_key}'
+        auth = base64.b64encode(msg.encode('ascii')).decode('ascii')
 
-    url = config['access-token-url']
-    headers = {}
-    headers['Authorization'] = f'Basic {auth}'
-    headers['Content-type'] = 'application/json'
+        url = config['access-token-url']
+        headers = {}
+        headers['Authorization'] = f'Basic {auth}'
+        headers['Content-type'] = 'application/json'
 
-    LOGGER.debug('Authenticating with client credentials')
+        scope = kwargs.pop('scope', 'aps')
+        response = ApsRequest.post(url, headers=headers, json={'scope': scope})
+        resp = response.json()
 
-    scope = kwargs.pop('scope', 'aps')
-    response = ApsRequest.post(url, headers=headers, json = {'scope': scope})
-    resp = response.json()
-    #LOGGER.debug(f'Get access token response: {resp}')
+    LOGGER.debug(f'Get access token response: {resp}')
 
     if not 'token' in resp:
-        LOGGER.error('Failed to authenticate, please check client ID and client secret value')
-        raise ApsException('Failed to authenticate, please check client ID and client secret value')
+        LOGGER.error(
+            'Failed to authenticate, please check client ID and client secret value')
+        raise ApsException(
+            'Failed to authenticate, please check client ID and client secret value')
     return f'Bearer {resp["token"]}'
