a4dd37fa652d583e84a3f7369846bf5ed5c073a7
Johan Lindquist <jlindquist@verimatrix.com>
2023-11-15 11:15:59 -0500

    fix: moved platform handling to earlier in the file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,18 @@
 
 # retrieving account info for getting subscription_type if it's not provided
 
-if [ -z "${SUBSCRIPTION_TYPE}" ]; then
-  python3 /aps-cli/aps.py -l=DEBUG -c "$API_KEY_ID" -s "$API_SECRET" --api-gateway-url "$API_GATEWAY_URL" --access-token-url "$ACCESS_TOKEN_URL" get-account-info  > account.info
-  SUBSCRIPTION=$(cat account.info | jq -r '.["customer"]["subscriptions"][0]["type"]')
-  echo "Subscription Type retrieved [${SUBSCRIPTION}]"
-else
-  SUBSCRIPTION="${SUBSCRIPTION_TYPE}";
-fi
 
 ENABLE_PLATFORM=""
 if [ "${PLATFORM}" != "" ] ; then
   ENABLE_PLATFORM="-P"
+fi
+
+if [ -z "${SUBSCRIPTION_TYPE}" ]; then
+  python3 /aps-cli/aps.py -l=DEBUG ${ENABLE_PLATFORM} -c "$API_KEY_ID" -s "$API_SECRET" --api-gateway-url "$API_GATEWAY_URL" --access-token-url "$ACCESS_TOKEN_URL" get-account-info  > account.info
+  SUBSCRIPTION=$(cat account.info | jq -r '.["customer"]["subscriptions"][0]["type"]')
+  echo "Subscription Type retrieved [${SUBSCRIPTION}]"
+else
+  SUBSCRIPTION="${SUBSCRIPTION_TYPE}";
 fi
 
 if [ -n "${SUBSCRIPTION}" ]; then
